66187e3dd0a4eaa1994a9e21fac771007a453c11
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-12-19 15:18:34 +0700

    benefits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Benefits from "./_components/benefits";
 import Hero from "./_components/hero";
 
 export default function HomePage() {
   return (
     <div className="space-y-20 py-20">
       <Hero />
+      <Benefits />
     </div>
   );
 }
