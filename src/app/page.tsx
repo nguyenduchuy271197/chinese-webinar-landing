877ab41d5379efeb13303b7eecbd8a87b8f3ad6a
tuanphucnguyen2110@gmail.com <119840033+Fugena22@users.noreply.github.com>
2023-12-19 15:54:19 +0700

    features:sticky-bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Benefits from "./_components/benefits";
 import Hero from "./_components/hero";
+import StickyBar from "./_components/sticky-bar";
 
 export default function HomePage() {
   return (
     <div className="space-y-20 py-20">
       <Hero />
       <Benefits />
+      <StickyBar />
     </div>
   );
 }
