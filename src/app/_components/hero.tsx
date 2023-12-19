034b17372366041a3b80e337a4adb3ba49c7b20e
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-12-19 10:52:51 +0700

    initial files

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-[400px_1fr]">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+}
