2f191710755bd37f9671450450db75543dc5ce99
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-12-20 09:19:03 +0700

    update

---
+++ src/app/_components/story-chart.tsx
@@ -0,0 +1,3 @@
+export default function StoryChart() {
+  return <div>StoryChart</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Benefits from "./_components/benefits";
 import Hero from "./_components/hero";
 import StickyBar from "./_components/sticky-bar";
 import Story from "./_components/story";
+import StoryChart from "./_components/story-chart";
 
 export default function HomePage() {
   return (
@@ -9,6 +10,7 @@ export default function HomePage() {
       <Hero />
       <Benefits />
       <Story />
+      <StoryChart />
       <StickyBar />
     </div>
   );
